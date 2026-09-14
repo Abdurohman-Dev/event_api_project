eca69c8948340306e6d4031f48956c419b9d0fa6
Abdurohman <su1600439@su.edu.et>
2026-09-14 21:57:45 +0300

    Add custom validation logic for ticket cpacity in BookingSerializer

--- events/serializers.py
+++ events/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import UserProfile, Booking, Event
+from django.db.models import Sum
 
 class UserProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
@@ -20,3 +21,13 @@ class BookingSerializer(serializers.ModelSerializer):
         model = Booking
         fields = ['id','user','event','tickets_booked','booking_date','status']
         read_only_fields = ['user', 'booking_date']
+
+    def validate(self, data):
+        event = data.get('event')
+        tickets_booked = data.get('tickets_booked')
+        booked_tickets = Booking.objects.filter(event=event)
+        total_tickets = booked_tickets.aggregate(Sum('tickets_booked'))['tickets_booked__sum'] or 0
+        remaining_tickets = event.total_tickets - total_tickets
+        if tickets_booked > remaining_tickets:
+            raise serializers.ValidationError("በቂ ቲኬት የለም...!")
+        return data
