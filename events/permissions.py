bb68ff89a688cbc1e8172f4bb7bcacc4dadb5b82
Abdurohman <su1600439@su.edu.et>
2026-09-17 21:25:47 +0300

    Feat: Add isOwnerOrReadOnly and IsAdminOrReadOnly custom permissions

--- events/permissions.py
+++ events/permissions.py
@@ -5,3 +5,17 @@ class IsOrganizerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.organizer == request.user
+
+    
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.organizer == request.user
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool( request.user and request.user.is_staff)
